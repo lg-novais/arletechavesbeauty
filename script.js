2c3fdae0120eddc12d446552be5a72e15dd17d38
Luiz Gustavo <lgnovais07@gmail.com>
2025-03-14 15:20:40 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,38 @@
+//MENU
+const menuToggle = document.getElementById('menuToggle');
+const sidebar = document.getElementById('sidebar');
+const overlay = document.createElement('div');
+overlay.className = 'overlay';
+document.body.appendChild(overlay);
+
+// Abrir e fechar o menu ao clicar no botão hambúrguer
+menuToggle.addEventListener('click', function() {
+    this.classList.toggle('open');
+    sidebar.classList.toggle('open');
+    overlay.style.display = sidebar.classList.contains('open') ? 'block' : 'none';
+});
+
+// Fechar o menu ao clicar em um link de âncora
+document.querySelectorAll('.sidebar a').forEach(anchor => {
+    anchor.addEventListener('click', () => {
+        menuToggle.classList.remove('open');
+        sidebar.classList.remove('open');
+        overlay.style.display = 'none';
+    });
+});
+
+// Fechar o menu ao clicar fora dele
+overlay.addEventListener('click', () => {
+    menuToggle.classList.remove('open');
+    sidebar.classList.remove('open');
+    overlay.style.display = 'none';
+});
+
+/*Trocar a cor do logo */
+let mediaQuery = window.matchMedia("(max-width: 576px)").matches;
+const logo = document.querySelector('.logo img');
+    if(mediaQuery){
+        logo.setAttribute('src', 'imgs/logo/logo-branco.png'); 
+    } else{
+        logo.setAttribute('src', 'imgs/logo/logo.png');
+    }
